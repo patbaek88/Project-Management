a663540363c6c1d1ad23f63b794514a86179dd62
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2024-06-28 15:01:28 +0900

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ if password_input == "cmcpl":
     st.write("Step 2: Upload your project timeline file")
     uploaded_file = st.file_uploader('Fill out the project timeline template and upload your file here.', type=['csv'])
     if uploaded_file is not None:
-        df = pd.DataFrame(data = uploaded_file)
+        df = pd.read_csv(uploaded_file)
         st.write(df)
         st.write("")
         st.write("Step 3: Generate a Gantt chart")
