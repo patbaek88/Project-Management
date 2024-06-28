58e42d79515290972c74e0a7c467e2087d7e1ec0
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2024-06-28 16:01:13 +0900

    Update app.py

--- app.py
+++ app.py
@@ -22,21 +22,7 @@ if password_input == "cmcpl":
         st.write("")
         option = st.selectbox("View Gantt Chart by:", ("Team", "Completion %"))
         st.write("Step 3: Generate a Gantt chart")
-        if st.button("Generate a Gantt chart"):
-            # 데이터셋 만들기
-            #df = pd.DataFrame(data=data)
-            # 시작일과 종료일의 최소값과 최대값 구하기
-            min_date = pd.to_datetime(df['Start']).min()
-            max_date = pd.to_datetime(df['Finish']).max()
-
-            # 월별 세로줄 생성
-            vlines = []
-            for year in range(min_date.year, max_date.year+1):
-                for month in range(1, 13):
-                    date = pd.to_datetime(f'{year}-{month}-01')
-                    if date >= min_date and date <= max_date:
-                        vlines.append(dict(x=date, line_width=1, line_dash="dash", line_color="gray"))
-
+        if st.button("Generate a Gantt chart"):       
             # 데이터프레임을 Resource와 Start 열을 기준으로 정렬
             df = df.sort_values(['Resource', 'Start'])            
             # Gantt Chart 만들기
@@ -45,12 +31,27 @@ if password_input == "cmcpl":
             elif option == "Completion %":
                 fig = px.timeline(df, x_start="Start", x_end="Finish", y="Task", color="Completion_pct")
 
-            # x축 범위 설정
-            #fig.update_layout(xaxis_range=[df['Start'].min(), df['Finish'].max()])
-        
-            # 월별 세로줄 추가
-            for vline in vlines:
-                fig.add_vline(**vline)
+            # 세로 그리드 선 설정 (매월)
+            fig.update_xaxes(showgrid=True, gridwidth=1, gridcolor='LightGray')
+
+            # 레이아웃 업데이트 (Start의 최소값부터 Finish의 최대값까지 그리드 선을 그리기 위해)
+            fig.update_layout(
+                xaxis=dict(
+                    range=[df['Start'].min(), df['Finish'].max()],
+                    tick0=df['Start'].min(),
+                    dtick="M1",  # 그리드 선 간격 설정 (매월)
+                    minor=dict(
+                        tick0=df['Start'].min(),
+                        dtick="M1"
+                    )
+                )
+            )
+
+            fig.update_xaxes(
+                dtick="M1",  # 매월 표시
+                tickformat="%Y-%m-%d",  # 날짜 형식 설정
+                tickangle=45  # 날짜 라벨 각도 설정
+            )
     
 
             # 위에서부터 시작하게 Y축 역방향으로 설정
