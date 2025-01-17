e87b5ddbb814746850b34fc8e52bec71aa77081b
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2025-01-17 10:23:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -29,8 +29,14 @@ if password_input == "cmcpl":
             # Gantt Chart 만들기
             if option == "Team":
                 fig = px.timeline(df, x_start="Start", x_end="Finish", y="Task", color="Resource")
+                # 막대 위에 'Task' 이름 추가 (text 속성 사용)
+                fig.update_traces(text=df['Task'], textposition="top center")  # textposition="top center"로 막대 위에 텍스트 위치 지정
+
             elif option == "Completion %":
                 fig = px.timeline(df, x_start="Start", x_end="Finish", y="Task", color="Completion_pct")
+                # 막대 위에 'Task' 이름 추가 (text 속성 사용)
+                fig.update_traces(text=df['Task'], textposition="top center")  # textposition="top center"로 막대 위에 텍스트 위치 지정
+
 
             fig.update_xaxes(
                 dtick="M1",  # 매월 표시
@@ -67,27 +73,13 @@ if password_input == "cmcpl":
                 )
             )
     
-            # 굵은 세로막대 표시를 위한 함수 정의
-            def draw_bold_bars(fig, df, bold_interval):
-                for idx, row in df.iterrows():
-                    if (idx // bold_interval) % 2 == 0:  # 3개월 간격마다 굵은 막대 (bold_interval = 12 / 3)
-                        fig.add_shape(
-                            type="rect",
-                            x0=row['Start'],
-                            x1=row['Finish'],
-                            y0=row['Task'],
-                            y1=row['Task'] + 0.1,  # y 축의 상하 간격 조정 가능
-                            line=dict(color="RoyalBlue", width=2),  # 굵기와 색상 설정
-                            fillcolor='rgba(100,100,100,0.3)'  # 채우기 색 설정 (투명도 포함)
-                        )
             # 위에서부터 시작하게 Y축 역방향으로 설정
             fig.update_yaxes(autorange="reversed")
             #fig.show()
             #st.set_option('deprecation.showPyplotGlobalUse', False)
             st.plotly_chart(fig)
 
-            # GANTT 차트 생성 부분 위에 함수 호출 추가
-            draw_bold_bars(fig, df, 4)  # bold_interval은 3개월 간격
+
     else:
         st.write("Please upload your file")
 else:
